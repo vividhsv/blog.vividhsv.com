3f4e9b35fe1bd53164dad896ac6457df58f6a72d
Vividh S Viswanatha <vividhsv@gmail.com>
2016-05-14 00:06:55 -0400

    Make file s3_upload to use awscli rather than s3cmd

--- publishconf.py
+++ publishconf.py
@@ -11,6 +11,8 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 SITEURL = 'http://blog.vividhsv.com'
+SITELOGO = SITEURL + '/images/vividhsv.png'
+FAVICON = SITEURL + '/images/favicon.ico'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
